f656aecd6debe091ff4ba7c5a6c03cdb3e7161cd
sxfxtf@sina.com <sxfxtf@sina.com>
2021-02-08 20:23:36 +0800

    更新路飞商城.py

--- 路飞商城.py
+++ 路飞商城.py
@@ -5,6 +5,7 @@ from selenium.common import exceptions
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from lxml import etree
+import os
 
 login_url = 'https://www.luffycity.com/study/degree'
 br = webdriver.Chrome()
@@ -12,7 +13,8 @@ br.get(url=login_url)
 br.find_element_by_xpath("//div[@class='username inputcontain']/input").send_keys("15867416745")
 br.find_element_by_xpath("//div[@class='password inputcontain']/input").send_keys("192406")
 # br.find_element_by_xpath("//div[@class='signin']/input").send_keys("192406")
-
+t = " "
+end_str = "1.55 socketserver源码解析2"
 while 1:
     print("进入第一个循环")
     flag = 0
@@ -26,10 +28,20 @@ while 1:
         print("找到课程播放框,开始监控......")
         page = br.page_source
         tree = etree.HTML(page)
+
+        if t == end_str:
+            br.quit()
+            os.system("shutdown -s -t 5")
+            os.system("taskkill -im pycharm64.exe")
+            # exit("程序退出了")
+        else:
+            print("不是 %s, 继续运行"%end_str)
+
         titile = tree.xpath("//div[@class='section active']/section[1]/h5/text()")
         play_time = tree.xpath("//div[@class='section active']/section[2]/span/text()")
         print(play_time)
         print("%s 视频,一共播放时长为 %s" % (titile[0], play_time[0]))
+        t = titile[0]
         wait_time = play_time[0].split(":")[0]
         flag = 1
         print("等待时间为%s分钟" % wait_time)
